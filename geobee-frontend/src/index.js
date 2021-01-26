be5f37ff045748afe3e49b6fdd26bee82627550a
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-01-26 10:48:53 -0800

    functioning counter, working on hash mess

--- geobee-frontend/src/index.js
+++ geobee-frontend/src/index.js
@@ -7,29 +7,83 @@ const USER_URL = `${BASE_URL}/users`
 // addMap() 
 let score = 0
 
+// const states = [
+//     {'AL' => "Alabama"},
+//     {'AK' => "Alaska"},
+//     {'AZ' => "Arizona"},
+//     {'AR' => "Arkansas"},
+//     {'CA' "California"},
+//     {'CO' "Colorado"},
+//     {'CT' "Connecticut"},
+//     {'DE' "Delaware"},
+//     {'FL' "Florida"},
+//       'GA' "Georgia"},
+//       'HI' "Hawaii"}, 
+//       'ID' "Idaho"},
+//       'IL' "Illinois"},
+//       'IN' "Indiana"},
+//       'IA' "Iowa"},
+//       'KS' "Kansas"},
+//       'KY' "Kentucky"},
+//       'LA' "Louisiana"},
+//       'ME' "Maine"},
+//       'MD' "Maryland"}, 
+//       'MA' "Massachusetts"}, 
+//       'MI' "Michigan"},
+//       'MN' "Minnesota"},
+//       'MS' "Mississippi"},
+//       'MO' "Missouri"},
+//       'MT' "Montana"},
+//       'NE' "Nebraska"},
+//       'NV' "Nevada"},
+//       'NH' "New Hampshire"},
+//       'NJ' "New Jersey"},
+//       'NM' "New Mexico"},
+//       'NY' "New York"},
+//       'NC' "North Carolina"},
+//       'ND' "North Dakota"},
+//       'OH' "Ohio"},
+//       'OK' "Oklahoma"},
+//       'OR' "Oregon"},
+//       'PA' "Pennsylvania"},
+//       'RI' "Rhode Island"}, 
+//       'SC' "South Carolina"},
+//       'SD' "South Dakota"}, 
+//       'TN' "Tennessee"},
+//       'TX' "Texas"},
+//     'UT' "Utah"},
+//     'VT' "Vermont"},
+//     'VA' "Virginia"},
+//     'WA' "Washington"},
+//     'WV' "West Virginia"},
+//     'WI' "Wisconsin"},
+//     'WY' "Wyoming"},
+// }
+
 function handleClick(state) {
     console.log(state.name)
-    form = document.getElementById('state-input')
+    let div = document.getElementById('state-form-block')
+    div.innerHTML = ''
+    let form = document.createElement('form')
+    let input = document.createElement('input')
+    let submit = document.createElement('input')
     form.innerHTML = ''
-    input = document.createElement('input')
     input.type = "text"
     input.name = "state"
-    submit = document.createElement('input')
     submit.type = "submit"
     submit.value = "Submit" 
     form.addEventListener('submit', (e) => handleSubmit(e, state))
 
+    div.appendChild(form)
     form.appendChild(input)
     form.appendChild(submit)
 }
 
 function handleSubmit(e, state) {
     e.preventDefault()
-    scoreKeeper = document.getElementById('score-keeper')
-    if (e.target.state.value == state.name) {
+    let scoreKeeper = document.getElementById('score-keeper')
+    if (e.target.state.value === state.name) {
         score++
-        scoreKeeper.innerText = `Current Score: ${score}`
-    } else {
         scoreKeeper.innerText = `Current Score: ${score}`
     }
 }
