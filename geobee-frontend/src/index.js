03a4e49fa300fb90eab0d55627c5d96bb2b8fb0f
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-01-31 19:33:43 -0800

    sets backend url

--- geobee-frontend/src/index.js
+++ geobee-frontend/src/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "https://geograbee-api.herokuapp.com"
 const STATESGAME_URL = `${BASE_URL}/states_games`
 const SCORE_URL = `${BASE_URL}/scores`
 const USER_URL = `${BASE_URL}/users`
